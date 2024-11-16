548571df2d1eef3197b230247d7409676ea19f22
Luis <luis@lu15.dev>
2024-11-16 11:46:42 +0000

    chore!(deps): update to latest version of Minecraft (#8)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- test/plugin/build.gradle.kts
+++ test/plugin/build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
     java
-    id("io.papermc.paperweight.userdev") version "1.7.1"
-    id("xyz.jpenilla.run-paper") version "2.3.0"
-    id("xyz.jpenilla.resource-factory-paper-convention") version "1.1.2"
-    id("io.github.goooler.shadow") version "8.1.7"
+    alias(libs.plugins.paper.paperweight)
+    alias(libs.plugins.paper.run)
+    alias(libs.plugins.paper.resource)
+    alias(libs.plugins.shadow)
 }
-
-val minecraftVersion: String by project
 
 java.toolchain.languageVersion = JavaLanguageVersion.of(21)
 
@@ -15,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    paperweight.paperDevBundle("$minecraftVersion-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("${libs.versions.minecraft.get()}-R0.1-SNAPSHOT")
 
     implementation(project(":"))
     implementation(project(path = ":paper-codec-test-shared", configuration = "namedElements"))
